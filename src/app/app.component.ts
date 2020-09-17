1718f966e1480431829ab16193947d5cfc500969
Jorge Leiva <66340011+jorgehleiva@users.noreply.github.com>
2020-09-17 15:53:57 -0300

    copaLibertadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'copaLibertadores';
+  title = 'copaLibertadores - deploy a Github Pages';
 }
